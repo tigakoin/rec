d0adb7e9f8c70fdb33d16e69f8705deb2f94e769
tigakoin <42860390+tigakoin@users.noreply.github.com>
2018-09-05 08:46:55 +0700

    Add files via upload

---
+++ rec.js
@@ -0,0 +1,1 @@
+function artikelterbaru(e){document.write("<ul>");for(var t=0;t<e.feed.entry.length;t++){for(var r=0;r<e.feed.entry[t].link.length&&"alternate"!=e.feed.entry[t].link[r].rel;r++);var n="<li><a href="+("'"+e.feed.entry[t].link[r].href+"'")+'">'+e.feed.entry[t].title.$t+"</a> </li>";document.write(n)}document.write("</ul>")}
